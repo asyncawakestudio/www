6a70ab5cd4e4af0dbec4f3c2339c78e70f8e8e43
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2026-01-19 22:54:54 +0530

    chore: update content

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,13 +3,10 @@ import { Box, Link, List, ListItem, Typography } from "@mui/material";
 export default function Home() {
     return (
         <Box>
-            <Typography data-testid="home-title" style={{ textAlign: 'left' }} variant="h4">Async Awake</Typography>
+            <Typography data-testid="home-title" style={{ textAlign: 'left' }} variant="h4">Async Awake Studio</Typography>
             <br />
-            <Typography data-testid="home-subtitle" style={{ textAlign: 'left' }} variant='subtitle1'>
-                A technology blog about coding, software engineering, cyber security and more.
-            </Typography>
-            <Typography data-testid="home-content" style={{ display: "none" }} variant='subtitle1'>
-                We build accessible, performant reaponsive and secure applications.
+            <Typography data-testid="home-content" style={{ textAlign: 'left' }} variant='subtitle1'>
+                We build accessible, performant, responsive and secure applications.
             </Typography>
             <List>
                 <ListItem style={{ paddingLeft: 0 }}>
