737d2bad55bad7b06d6a18063f6e71049cc2db9d
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2026-02-08 12:42:34 +0530

    chore(content): add async awake specific content

--- src/screens/Post.tsx
+++ src/screens/Post.tsx
@@ -3,19 +3,7 @@ import Markdown from 'react-markdown';
 import { useParams } from "react-router-dom";
 
 import {
-    IMG_20251001_1,
-    IMG_20251001_2,
-    IMG_20251001_3,
-    IMG_20251001_4,
-    IMG_20251001_5,
-    IMG_20251001_6
-} from '../assets/index';
-import {
-    POST_20241228,
-    POST_20250401,
-    POST_20250801,
-    POST_20250901,
-    POST_20251001,
+    POST_20260301,
     postsInfo,
 } from "../content/index";
 
@@ -23,47 +11,13 @@ export default function Post() {
     const { date } = useParams();
 
     const posts: Record<string, string> = {
-        '2024-12-28.md': POST_20241228,
-        '2025-04-01.md': POST_20250401,
-        '2025-08-01.md': POST_20250801,
-        '2025-09-01.md': POST_20250901,
-        '2025-10-01.md': POST_20251001,
-    };
-
-    const images: Record<string, string> = {
-        '2025-10-01_1.jpeg': IMG_20251001_1,
-        '2025-10-01_2.jpeg': IMG_20251001_2,
-        '2025-10-01_3.jpeg': IMG_20251001_3,
-        '2025-10-01_4.jpeg': IMG_20251001_4,
-        '2025-10-01_5.jpeg': IMG_20251001_5,
-        '2025-10-01_6.jpeg': IMG_20251001_6,
+        '2026-03-01.md': POST_20260301,
     };
 
     return (
         <Box>
             <div style={{ textAlign: 'left' }}>
-                <Markdown
-                    components={{
-                        img: ({ src, alt, ...props }) => {
-                            const resolvedSrc = images[src as string]; // get imported asset
-                            if (!resolvedSrc) {
-                                console.warn(`Image not found: ${src}`);
-                            }
-                            return (
-                                <img
-                                    {...props}
-                                    src={resolvedSrc || src}
-                                    alt={alt}
-                                    style={{
-                                        maxHeight: 400,
-                                        maxWidth: '100%',
-                                        border: '1px solid #ccc',
-                                    }}
-                                />
-                            );
-                        },
-                    }}
-                >
+                <Markdown>
                     {posts[date + '.md']}
                 </Markdown>
             </div>
