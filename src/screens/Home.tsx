4f14aa00d6a7c4886297816d488f96a0cebe40e5
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2026-02-17 13:34:42 +0530

    chore: update content

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -6,7 +6,7 @@ export default function Home() {
             <Typography data-testid="home-title" style={{ textAlign: 'left' }} variant="h4">Async Awake Studio</Typography>
             <br />
             <Typography data-testid="home-content" style={{ textAlign: 'left' }} variant='subtitle1'>
-                We build accessible, performant, responsive and secure applications.
+                We build SPARC (secure, performant, accessible, responsive and compliant) applications.
             </Typography>
             <List>
                 <ListItem style={{ paddingLeft: 0 }}>
